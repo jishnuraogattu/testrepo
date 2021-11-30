d0a14c4884f38b88e6fbf157fe379d88786bdfbe
jishnuraogattu <95121929+jishnuraogattu@users.noreply.github.com>
2021-11-30 11:46:48 +0530

    Add files via upload

---
+++ hello world a pro.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# ### GATTUJISHNU
+
+# ###### I LIKE TO CREATE A MODELS FOR REAL TIME ACESS OF DATA
+
+# I am interested in data science because it is an interesting and popular field where I can be useful
+
+# ### This code multiplies the number by itself four times
+
+# In[2]:
+
+
+def six_degree(a):
+    return a**6
+
+b = 2
+print(six_degree(b))
+b*b*b*b*b*b == six_degree(b)
+
+
+# ### This code add the numbers
+
+# In[3]:
+
+
+a =3 
+b = 4
+print(a+b)
+
+
+# |s.no| name  | gender   | age  |
+# |---:|:------|:---------|:-----|
+# | 1 | raja  | male   | 26   |
+# | 2 | guru   | male   | 34   | 
